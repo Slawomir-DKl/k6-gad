6ec7836282b6fa8fbcd8f1bdcae1020cb49fd083
slawekdk <slawomir@d-kl.pl>
2025-06-23 23:22:21 +0200

    feat: options  in separate file

--- src/options.ts
+++ src/options.ts
@@ -9,19 +9,59 @@ export function chooseOptions(
   optionsVariant: OptionsType,
   durationInSeconds: number = 10,
   vus: number = 5
-) {
+): Options {
   if (durationInSeconds <= 0) {
     throw new Error("Duration should be positive number");
   }
-  let options;
-  if ((optionsVariant = OptionsType.rampingVus)) {
-    options = [
-      { duration: "5s", target: 20 },
-      { duration: "5s", target: 20 },
-      { duration: "5s", target: 0 },
-    ];
-  } else {
-    options = [];
+
+  vus = Math.floor(vus);
+  if (vus < 1) {
+    throw new Error("Number of virtual users (vus) should be >= 1");
   }
+
+  let rampDuration = 0;
+  if (durationInSeconds > 10) {
+    rampDuration = Math.floor(durationInSeconds / 10);
+  } else if (durationInSeconds > 3) {
+    rampDuration = 1;
+  }
+
+  const maintenanceDuration = durationInSeconds - 2 * rampDuration;
+
+  const optionsMap: Record<OptionsType, Options> = {
+    [OptionsType.rampingVus]: {
+      thresholds: {
+        http_req_failed: ["rate<0.01"],
+        http_req_duration: ["p(99)<100"],
+      },
+      scenarios: {
+        average_load: {
+          executor: "ramping-vus",
+          stages: [
+            { duration: `${rampDuration}s`, target: vus },
+            { duration: `${maintenanceDuration}s`, target: vus },
+            { duration: `${rampDuration}s`, target: 0 },
+          ],
+        },
+      },
+    },
+
+    [OptionsType.constantVus]: {
+      scenarios: {
+        constant_load: {
+          executor: "constant-vus",
+          vus: vus,
+          duration: `${durationInSeconds}s`,
+        },
+      },
+    },
+  };
+
+  const options = optionsMap[optionsVariant];
+
+  if (!options) {
+    throw new Error(`Unknown options variant: ${optionsVariant}`);
+  }
+
   return options;
 }
--- tests/get-articles-200.perf.test.ts
+++ tests/get-articles-200.perf.test.ts
@@ -1,22 +1,10 @@
 import { check } from "k6";
 import { Options } from "k6/options";
 import http from "k6/http";
-import { chooseOptions, OptionsType } from "../src/options.ts";
 import { BASE_API_URL } from "../config.ts";
+import { chooseOptions, OptionsType } from "../src/options.ts";
 
-export const options: Options = {
-  thresholds: {
-    http_req_failed: ["rate<0.01"],
-    http_req_duration: ["p(99)<100"],
-  },
-  scenarios: {
-    // arbitrary name of scenario
-    average_load: {
-      executor: "ramping-vus",
-      stages: chooseOptions(OptionsType.rampingVus),
-    },
-  },
-};
+export const options: Options = chooseOptions(OptionsType.rampingVus);
 
 const endpoint = "articles";
 const params = "?_limit=6&_page=1&_sort=date&_order=DESC";
